0c9906b4d9ffc31c78968ccc4d51805f15e639f8
Cryo Blade <jzeta2011@gmail.com>
2025-12-26 13:40:23 -0500

    More logic, still missing word choosing

--- main.js
+++ main.js
@@ -1,7 +1,50 @@
 var currentPlayer;
 var totalPlayers = 4;
+var currentState;
+var chameleon;
 
 function setup() {
 	currentPlayer = 0;
+	currentState = 0;
 	document.getElementById("playerNum").innerHTML = currentPlayer;
+	chameleon = Math.floor(Math.random() * totalPlayers) + 1;
+};
+
+function getWord(playerNum) {
+	var word = "blah blah";
+	if(playerNum == chameleon) {
+		//give out fake word
+	}
+	else {
+		//give out real word
+	}
+	return word;
+}
+
+function buttonClick() {
+	if(currentPlayer == 0) {
+		currentPlayer = 1;
+		document.getElementById("playerNum").innerHTML = currentPlayer;
+		currentState = 1;
+		document.getElementById("currentWord").innerHTML = getWord(currentPlayer);
+		document.getElementById("actionButton").innerHTML = "Clear!";
+	}
+	else if(currentState == 1) {
+		currentPlayer++;
+		if (currentPlayer>totalPlayers) {
+			document.getElementById("action").innerHTML = "";
+		}
+		else {
+			document.getElementById("playerNum").innerHTML = currentPlayer;
+			currentState = 0;
+			document.getElementById("currentWord").innerHTML = "";
+			document.getElementById("actionButton").innerHTML = "Start!";
+		}
+	}
+	else {
+		document.getElementById("playerNum").innerHTML = currentPlayer;
+		currentState = 1;
+		document.getElementById("currentWord").innerHTML = getWord(currentPlayer);
+		document.getElementById("actionButton").innerHTML = "Clear!";
+	}
 };
